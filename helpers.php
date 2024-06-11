768120dabd30c149e5712465919c968e7e360677
Younes ENHARI <45436028+algoyounes@users.noreply.github.com>
2024-06-11 18:28:43 +0100

    tweak: add helpers file for utility functions

---
+++ helpers.php
@@ -0,0 +1,16 @@
+<?php
+
+if (! function_exists('make')) {
+    /**
+     * @template TClass
+     *
+     * @param  class-string<TClass>  $abstract
+     * @return TClass
+     *
+     * @throws \Illuminate\Contracts\Container\BindingResolutionException
+     */
+    function make(string $abstract, array $parameters = [])
+    {
+        return \Illuminate\Container\Container::getInstance()->make($abstract, $parameters);
+    }
+}
