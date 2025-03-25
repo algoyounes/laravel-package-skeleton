f601d0ad34b492d233b54b8edd98923e6af24fa2
Younes ENHARI <45436028+algoyounes@users.noreply.github.com>
2025-03-25 06:54:42 +0000

    refactor: extract config handling methods

--- src/Providers/SkeletonServiceProvider.php
+++ src/Providers/SkeletonServiceProvider.php
@@ -8,12 +8,26 @@ class SkeletonServiceProvider extends ServiceProvider
 {
     public function boot(): void
     {
-        $this->publishes([
-            dirname(__DIR__, 2).'/config/skeleton.php' => config_path('skeleton.php'),
-        ], 'config');
+        $this->publishConfig();
     }
 
     public function register(): void
+    {
+        $this->registerConfig();
+    }
+
+    // Boot methods :
+    private function publishConfig(): void
+    {
+        if ($this->app->runningInConsole()) {
+            $this->publishes([
+                dirname(__DIR__, 2).'/config/skeleton.php' => config_path('skeleton.php'),
+            ], 'config');
+        }
+    }
+
+    // Register methods :
+    private function registerConfig(): void
     {
         $this->mergeConfigFrom(dirname(__DIR__, 2).'/config/skeleton.php', 'skeleton');
     }
